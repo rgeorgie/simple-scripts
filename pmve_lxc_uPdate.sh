b26c32d7f9c6005d938261bd453f6601821bd0f9
rgeorgie <w0405552@nscc.ca>
2019-02-20 14:44:08 -0400

    Re - tested. Fixed the parsing issue

--- pmve_lxc_uPdate.sh
+++ pmve_lxc_uPdate.sh
@@ -1,20 +1,27 @@
-#!/usr/bin/env bash
+#! /bin/bash
 
 # update LXC containers under ProxmoxVE
 # Author: Rosen Georgiev a.k.a Subzer0
 # Date: Feb-12-2019
-# Tested with PVE 4.15.18-32 and deb containers
+# Re-Tested :) with PVE 4.15.18-32 and deb containers
 
-#listing all running containers
+# Don't do it if you're not tested the updates first!!!
+
+# listing all running containers
+
 for i in $(pct list | grep running |  awk '/[0-9]/{print $1}'); do
 
-echo -e "----------------------------\n--- for container '$i' ---\n----------------------------"
+# Updting them
 
-# update & upgrade
-pct exec "$i" apt update&&apt -y upgrade&&
+echo -e "----------------------------\n--- for container $i ---\n----------------------------"
+
+pct exec $i -- bash -c 'apt update' &&
+pct exec $i -- bash -c 'apt -y upgrade' &&
 
 echo -e "-----------------------------------\n- Done `date` -\n-----------------------------------\n"
 
 done
 
+
 #use on your own risk!!!
+# Subzer0
