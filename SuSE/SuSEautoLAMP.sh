dbb815d26e63c486b4e3866905320c807f0a4d8c
rgeorgie <w0405552@nscc.ca>
2019-05-10 12:43:42 -0300

    Apache, MariaDB, PHP

--- SuSE/SuSEautoLAMP.sh
+++ SuSE/SuSEautoLAMP.sh
@@ -7,14 +7,49 @@
 
 # get IP address
 IPaddress=$(ip route | grep src | cut -d " " -f 9)
+DatE=$(date)
 
 # install apache2
-zypper install apache2
 
-systemctl start apache2
+zypper install apache2 &&
 
-systemctl enable apache2
+#sed -i 'FW_CONFIGURATIONS_EXT=""/FW_CONFIGURATIONS_EXT="apache2"/g' /etc/sysconfig/SuSEfirewall2 TODO
 
-echo -e "<html>\n<body>\n<h1> It works!</h1>\n</body> \n</html>" > /srv/www/htdocs/index.html
+systemctl start apache2 &&
 
-echo "You may test the service on http://$IPaddress"
+systemctl enable apache2 &&
+
+echo -e "<html>\n<body>\n<h1> It works! $DatE Subzer0 </h1>\n</body> \n</html>" > /srv/www/htdocs/index.html
+echo -e "=================================================\n"
+echo "= You may test the service on http://$IPaddress ="
+echo -e "=================================================\n"
+
+chown wwwrun /srv/www/htdocs/ -R &&
+
+# install MariaDB
+
+zypper install mariadb mariadb-client mariadb-tools &&
+systemctl start mysql &&
+systemctl enable mysql &&
+
+echo -e "=================================================\n"
+echo "== Please complete the following MySQL setup =="
+echo -e "=================================================\n"
+
+mysql_secure_installation &&
+
+zypper install php7 php7-mysql apache2-mod_php7 &&
+
+a2enmod php7
+systemctl restart apache2
+
+echo "<?php phpinfo(); ?>" > /srv/www/htdocs/test.php &&
+echo -e "=========================================================\n"
+echo "= You may test PHP service on http://$IPaddress/test.php ="
+echo -e "=========================================================\n"
+
+# install phpMyAdmin TODO
+
+echo -e "================================================================\n"
+echo "= Installation DONE at $DatE additional setup necessary! Subzer0 ="
+echo -e "================================================================\n"
